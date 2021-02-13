589c75dfa532991b119aa3f3be50d047660722fe
James Gilley <jamesgilley1@icloud.com>
2021-02-13 12:45:34 -0600

    ..

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,137 @@ mongoose.connect("mongodb+srv://main:main@cluster0.gyqur.mongodb.net/main?retryW
 var express = require('express');
 var app = express();
 var path = require('path');
+app.use(express.urlencoded());
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
 
+
+/* //create userSchema and Model
+var userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  userEmail: String
+} ,
+{ timestamps: true });
+
+userSchema.method("toJSON", function() {
+   const { __v, _id, ...object } = this.toObject();
+   object.id = _id;
+   return object;
+ });
+
+var User = mongoose.model("User", userSchema);
+
+//user routes
+
+app.get("/users", (req, res ) => {
+  User.find().then(users => {
+      // console.log(users)
+      res.send(users)
+  })
+})
+
+app.delete("/users", (req, res) => {
+  User.deleteMany({}, ()=>{
+      console.log("all users deleted")
+      res.send({msg: "all uers deleted"})
+  })
+})
+
+app.post("/adduser", (req, res) => {
+  // console.log(req.body)
+  var myData = new User(req.body);
+  myData.save()
+      .then(item => {
+          res.send(item);
+      })
+      .catch(err => {
+          res.status(400).send("Unable to save to database");
+      });
+}); */
+
+//create workout Schema and Model (made of exercises)
+var workoutSchema = mongoose.Schema({
+  type: String,
+  name: String,
+  distance: String,
+  weight: String,
+  sets: String,
+  reps: String,
+  duration: String,
+
+} ,
+{ timestamps: true });
+
+workoutSchema.method("toJSON", function() {
+   const { __v, _id, ...object } = this.toObject();
+   object.id = _id;
+   return object;
+ });
+
+var Workout = mongoose.model("Workout", workoutSchema);
+
+// workout routes
+app.get("/workouts", (req, res ) => {
+  Workout.find({}).sort({createdAt: -1})
+  .then(workouts => {
+      console.log(workouts)
+      
+      res.send(workouts)
+  })
+})
+
+app.delete("/workouts", (req, res) => {
+  Workout.deleteMany({}, ()=>{
+      console.log("all workouts deleted")
+      res.send({msg: "all workouts deleted"})
+  })
+})
+
+app.post("/addworkout", (req, res) => {
+  console.log('server post /addworkout endpoint log.')
+  console.log(req.body)
+  var myData = new Workout(req.body);
+  myData.save()
+      .then(item => {
+          res.send(item);
+      })
+      .catch(err => {
+          res.status(400).send("Unable to save to database");
+      }); 
+});
+
+app.delete("/workout/:id", (req, res) => {
+  Workout.findByIdAndRemove(req.params.id, (error, data) => {
+      if (error) {
+          return (error);
+        } else {
+          console.log("workout deleted")
+          res.send({msg: "workout deleted"})
+        }
+  })
+})
+
+app.put("/workoutedit", (req, res) => {
+  let id = ""
+ // console.log('Edit happened2', req.body.name)
+  //Workout.findByIdAndUpdate(req.params.id, { $set: req.body}, (error, data) => {
+ Workout.findOne({name: req.body.name})
+.then(myData => {
+  Workout.findByIdAndUpdate(myData.id, req.body)
+ .then(data => console.log(data, "success!!!"))
+ .catch(err => console.log(err)) 
+})
+.catch(err => console.log(err))
+
+console.log(id, "<=== this is id")
+
+
+})
+
+
+//express static
 app.use(express.static(__dirname));
 
 app.use(express.static("public"));
@@ -26,34 +156,16 @@ app.get('/stats', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/stats.html'));
 });
 
-
-
-var nameSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String
+app.get('/update', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/update.html'));
 });
-var User = mongoose.model("User", nameSchema);
-user = User({
-firstName: 'My Name',
-lastName: 'lastName'
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    "running server!"
 })
-console.log(user)
- 
-user.save().then(() => console.log('Done'))
 
-
-
-
-app.get('/', function(req, res) {
-    res.send('/index.html');
-});
-
-app.get('/exercise', function(req, res) {
-    res.send('/exercise.html');
-});
-
-app.get('/stats', function(req, res) {
-    res.send('/stats.html');
-});
-
-app.listen(3000);
+//app.listen(3000, () => console.log("server is running!!!"));
